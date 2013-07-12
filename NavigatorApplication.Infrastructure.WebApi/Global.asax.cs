e627db88e4a79676abe95f3c44aa12a8cf56a82d
stuartshay <Yankees2013>
2013-07-12 19:57:52 -0400

    Project Config

--- NavigatorApplication.Infrastructure.WebApi/Global.asax.cs
+++ NavigatorApplication.Infrastructure.WebApi/Global.asax.cs
@@ -1,17 +1,16 @@
-﻿using System.Web;
-using System.Web.Http;
-using System.Web.Mvc;
-
-namespace NavigatorApplication.Infrastructure.WebApi
+﻿namespace NavigatorApplication.Infrastructure.WebApi
 {
-    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
-    // visit http://go.microsoft.com/?LinkId=9394801
+    using System.Web;
+    using System.Web.Http;
+    using System.Web.Mvc;
+    using NavigatorApplication.Infrastructure.WebApi.App_Start;
 
     public class WebApiApplication : HttpApplication
     {
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
         }
     }
 }
