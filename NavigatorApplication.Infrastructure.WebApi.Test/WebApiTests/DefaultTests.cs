c659a8cae3d27923e1c4df0b9b2f5722c2a1ed09
stuartshay <Yankees2013>
2013-07-12 21:35:28 -0400

    Added Web.Api Tests

---
+++ NavigatorApplication.Infrastructure.WebApi.Test/WebApiTests/DefaultTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using NUnit.Framework;
+using System.Net.Http;
+using System.Configuration;
+using System.Net;
+using System.Net.Http.Headers; 
+
+
+namespace NavigatorApplication.Infrastructure.WebApi.Test.WebApiTests
+{
+    [TestFixture]
+    public class DefaultTests
+    {
+        private HttpClient client;
+        private HttpResponseMessage response;
+
+        [SetUp]
+        public void SetUP()
+        {
+            client = new HttpClient();
+
+            client.BaseAddress = new Uri(ConfigurationManager.AppSettings["serviceBaseUri"]);
+            response = client.GetAsync("DefaultApi/get").Result;
+        }
+
+        [Test]
+        public void GetResponseIsSuccess()
+        {
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+
+
+        [Test]
+        public void GetResponseIsJson()
+        {
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            Assert.AreEqual("application/json", response.Content.Headers.ContentType.MediaType);
+        }
+
+        [Test]
+        public void GetAuthenticationStatus()
+        {
+            Assert.AreNotEqual(HttpStatusCode.Unauthorized, response.StatusCode);
+
+        } 
+    }
+}
--- NavigatorApplication.Infrastructure.WebApi/Controllers/DefaultApiController.cs
+++ NavigatorApplication.Infrastructure.WebApi/Controllers/DefaultApiController.cs
@@ -4,10 +4,39 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using NavigatorApplication.Infrastructure.WebApi.Model;
 
 namespace NavigatorApplication.Infrastructure.WebApi.Controllers
 {
     public class DefaultApiController : ApiController
     {
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        public PostResponse Post([FromBody]Person aPerson)
+        {
+            var responseObj = new PostResponse();
+            if (aPerson == null)
+            {
+                responseObj.ResponseText = "aPerson is null";
+                return responseObj;
+            }
+
+            if (aPerson.FirstName == null)
+            {
+                responseObj.ResponseText = "First Name is null";
+                return responseObj;
+            }
+
+            responseObj.ResponseText = string.Format("The first name is {0}", aPerson.FirstName);
+            return responseObj;
+        }
     }
 }
