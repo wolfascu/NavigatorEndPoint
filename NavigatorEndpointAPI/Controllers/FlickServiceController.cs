a6dbe4b7240b942f4261cbfd96fdcff6abef8a0c
tuanhuynh <hmtuan21@gmail.com>
2013-07-14 21:43:07 +1000

    First version

---
+++ NavigatorEndpointAPI/Controllers/FlickServiceController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.ServiceModel.Syndication;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml;
+using Raven.Client;
+using Raven.Client.Document;
+
+namespace NavigatorEndpointAPI.Controllers
+{
+    public class FlickServiceController : Controller
+    {
+        private DocumentStore store;
+
+        private IDocumentSession session { get; set; }
+        //
+        // GET: /FlickService/
+
+        public FlickServiceController()
+        {
+            var store = new DocumentStore()
+            {
+                Url = "http://localhost:8080/"
+            };
+            store.Initialize();
+            store.JsonRequestFactory.DisableRequestCompression = true;
+            session = store.OpenSession();
+        }
+
+        [AcceptVerbs(HttpVerbs.Get | HttpVerbs.Post)]
+        public string Index()
+        {
+            if(Request.RequestType == "GET")
+            {
+                var queryString = Request.QueryString;
+                var hubMode = queryString.Get("mode");
+                var hubTopic = queryString.Get("topic");
+                var challenge = queryString.Get("challenge");
+                var leaseSeconds = queryString.Get("lease_seconds");
+                var verifyToken = queryString.Get("queryString");
+                return challenge;
+            } else if(Request.RequestType == "POST")
+            {
+                var stream = Request.InputStream;
+                using(XmlTextReader xmlTextReader = new XmlTextReader(stream))
+                {
+                    var syncdicationFeed = SyndicationFeed.Load(xmlTextReader);
+                    session.Store(syncdicationFeed);
+                }              
+            }
+            return "ok";
+        }
+
+    }
+}
---
+++ NavigatorEndpointAPI/Controllers/FlickrService1Controller.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.ServiceModel.Syndication;
+using System.Web.Http;
+
+namespace NavigatorEndpointAPI.Controllers
+{
+    public class FlickrService1Controller : ApiController
+    {        
+
+        public FlickrService1Controller()
+        {
+            
+        }
+
+        public HttpResponseMessage Get()
+        {
+            var queryString = Request.RequestUri.ParseQueryString();
+            var hubMode = queryString.Get("mode");
+            var hubTopic = queryString.Get("topic");
+            var challenge = queryString.Get("challenge");
+            var leaseSeconds = queryString.Get("lease_seconds");
+            var verifyToken = queryString.Get("queryString");
+            var response = Request.CreateResponse();
+            response.Content = new StringContent(challenge);
+            return response;
+        }
+
+        public HttpResponseMessage Post([FromBody]string content)
+        {
+            var response = Request.CreateResponse();
+            response.Content = new StringContent("ok");
+            return response;
+        }
+    }
+}
