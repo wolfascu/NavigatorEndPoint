f9cbc2c3eb2385ef39ea5146fb30113ea032d9d0
Aleksander <aleksanderzinewicz@gmail.com>
2013-08-16 00:28:58 +0300

    Merged with master

--- NavigatorApplication.Infrastructure.WebApi/Global.asax.cs
+++ NavigatorApplication.Infrastructure.WebApi/Global.asax.cs
@@ -1,58 +1,28 @@
-<<<<<<< HEAD
 ﻿namespace NavigatorApplication.Infrastructure.WebApi
-{
-    using NavigatorApplication.Infrastructure.WebApi.App_Start;
-    using NavigatorApplication.Service.Registry;
-    using System.Web;
-    using System.Web.Http;
-    using System.Web.Mvc;
-    using System.Web.Routing;
-    using System.Web.Optimization;
+﻿{
+﻿    using NavigatorApplication.Infrastructure.WebApi.App_Start;
+﻿    using NavigatorApplication.Service.Registry;
+﻿    using System.Web;
+﻿    using System.Web.Http;
+﻿    using System.Web.Mvc;
+﻿    using System.Web.Routing;
+﻿    using System.Web.Optimization;
 
-    public class WebApiApplication : HttpApplication
-    {
-        protected void Application_Start()
-        {
-            log4net.Config.XmlConfigurator.Configure();
-            AreaRegistration.RegisterAllAreas();
-            WebApiConfig.Register(GlobalConfiguration.Configuration);
+﻿    public class WebApiApplication : HttpApplication
+﻿    {
+﻿        protected void Application_Start()
+﻿        {
+﻿            log4net.Config.XmlConfigurator.Configure();
+﻿            AreaRegistration.RegisterAllAreas();
+﻿            WebApiConfig.Register(GlobalConfiguration.Configuration);
 
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
-            FormatterConfig.RegisterFormatters(GlobalConfiguration.Configuration.Formatters);
+﻿            RouteConfig.RegisterRoutes(RouteTable.Routes);
+﻿            BundleConfig.RegisterBundles(BundleTable.Bundles);
+﻿            FormatterConfig.RegisterFormatters(GlobalConfiguration.Configuration.Formatters);
 
-            AutomapperRegistry.Configure();
+﻿            AutomapperRegistry.Configure();
 
-            Bootstrapper.Initialise();
-        }
-    }
-=======
-﻿namespace NavigatorApplication.Infrastructure.WebApi
-{
-    using NavigatorApplication.Infrastructure.WebApi.App_Start;
-    using NavigatorApplication.Service.Registry;
-    using System.Web;
-    using System.Web.Http;
-    using System.Web.Mvc;
-    using System.Web.Routing;
-    using System.Web.Optimization;
-
-    public class WebApiApplication : HttpApplication
-    {
-        protected void Application_Start()
-        {
-            log4net.Config.XmlConfigurator.Configure();
-            AreaRegistration.RegisterAllAreas();
-            WebApiConfig.Register(GlobalConfiguration.Configuration);
-
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
-            FormatterConfig.RegisterFormatters(GlobalConfiguration.Configuration.Formatters);
-
-            AutomapperRegistry.Configure();
-
-            Bootstrapper.Initialise();
-        }
-    }
->>>>>>> master
-}
+﻿            Bootstrapper.Initialise();
+﻿        }
+﻿    }
+﻿}
