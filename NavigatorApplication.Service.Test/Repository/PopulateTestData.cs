e86d4206476998222225b948e4ebef48e749a977
Cristi Lupascu <cristi.lupascu@gmail.com>
2013-08-27 21:48:35 +0300

    added RavenDB sample data population tests

---
+++ NavigatorApplication.Service.Test/Repository/PopulateTestData.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using System.Xml.Serialization;
+using AutoMapper;
+using NUnit.Framework;
+using NavigatorApplication.Service.DTO.Flickr;
+using NavigatorApplication.Service.RavenDbDocuments;
+using Raven.Client;
+using Raven.Client.Document;
+using Raven.Client.Embedded;
+
+namespace NavigatorApplication.Service.Test.Repository
+{
+    [TestFixture, Explicit]
+    public class PopulateTestData
+    {
+        [Test]
+        public void PopulateDataInStandaloneDatabase()
+        {
+            PopulateDataFromXml(new DocumentStore { ConnectionStringName = "RavenDb" }.Initialize());
+        }
+
+        [Test]
+        public void EmbeddedRavenDb_InMemory_Example()
+        {
+            var embeddedRamDocumentStore = new EmbeddableDocumentStore
+                {
+                    RunInMemory = true
+                }.Initialize();
+
+            using (embeddedRamDocumentStore)
+            {
+                PopulateDataFromXml(embeddedRamDocumentStore);
+
+                using (var session = embeddedRamDocumentStore.OpenSession())
+                {
+                    var feeds = session.Query<Feed>().ToList();
+
+                    Console.WriteLine("Read {0} feed(s) from the volatile embedded database. Their Ids and Titles are:");
+
+                    foreach (var feed in feeds)
+                        Console.WriteLine("\t{0}: {1}", feed.Id, feed.Title);
+                }
+            }
+        }
+
+        [Test]
+        public void EmbeddedRavenDb_OnDisk_Example()
+        {
+            var embeddedDiskDocumentStore = new EmbeddableDocumentStore
+                {
+                    RunInMemory = false,
+                    DataDirectory = "Data"
+                }.Initialize();
+
+            using (embeddedDiskDocumentStore)
+            {
+                PopulateDataFromXml(embeddedDiskDocumentStore);
+
+            }
+        }
+
+
+
+        public static void PopulateDataFromXml(IDocumentStore documentStore)
+        {
+            var dataFromXml = new XmlDataReader().ReadXmls().ToList();
+
+            var feedDocuments = dataFromXml.Select(Mapper.DynamicMap<Feed>).ToList();
+
+            using (var session = documentStore.OpenSession())
+            {
+                foreach (var feed in feedDocuments)
+                    session.Store(feed);
+
+                session.SaveChanges();
+            }
+        }
+
+
+
+
+        internal class XmlDataReader
+        {
+            private const string XmlPath = "../../../XML";
+
+            public IEnumerable<FlickrFeed> ReadXmls()
+            {
+                //var file = new FileInfo(Path.Combine(XmlPath, "1376803613.xml"));
+
+                var deserializer = new XmlSerializer(typeof(FlickrFeed));
+
+                foreach (var file in Directory.EnumerateFiles(XmlPath, "*.xml"))
+                {
+                    yield return (FlickrFeed)deserializer.Deserialize(new XmlTextReader(file));
+                }
+            }
+        }
+    }
+}
---
+++ NavigatorApplication.Service/RavenDbDocuments/Feed.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace NavigatorApplication.Service.RavenDbDocuments
+{
+    public class Feed
+    {
+        public string Id { get; set; }
+
+        public string Title { get; set; }
+
+        public DateTime UpdatedDate { get; set; }
+
+        public List<Entry> Entries { get; set; }
+    }
+
+    public class Entry
+    {
+        public string Id { get; set; }
+
+        public string Title { get; set; }
+
+        public DateTime PublishedDate { get; set; }
+
+        public DateTime UpdatedDate { get; set; }
+
+        public Content Content { get; set; }
+
+        public Author Author { get; set; }
+
+        public List<Category> Categories { get; set; }
+
+        public string WoeId { get; set; }
+
+        public double Latitude { get; set; }
+
+        public double Longitude { get; set; }
+
+        public string Point { get; set; }
+
+        public DateTime DateTaken { get; set; }
+
+        public string Safety { get; set; }
+
+        //public List<FlickrUpdate> Updates { get; set; }
+
+        public string MediaTitle { get; set; }
+
+        //public MediaCredit MediaCredit { get; set; }
+
+        //public MediaContent MediaContent { get; set; }
+
+        //public List<MediaThumbnail> MediaThumbnails { get; set; }
+
+        //public List<Link> Links { get; set; }
+    }
+
+    public class Content
+    {
+        public string Type { get; set; }
+        public string Body { get; set; }
+    }
+    public class Author
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public string Id { get; set; }
+        public string IconUrl { get; set; }
+    }
+    public class Category
+    {
+        public string Term { get; set; }
+        public string Scheme { get; set; }
+    }
+}
